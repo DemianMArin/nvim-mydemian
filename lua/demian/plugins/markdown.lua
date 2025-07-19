bfad5631d801bf0f305ccd55dcd66e9beb955218
DemianMArin <demianmarin03@gmail.com>
2025-07-18 22:36:46 -0600

    Had to modify installation type for markdown

--- lua/demian/plugins/markdown.lua
+++ lua/demian/plugins/markdown.lua
@@ -1,18 +1,10 @@
--- Markdown 
+-- Markdown
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-        vim.g.mkdp_auto_start = 0
-        vim.g.mkdp_auto_close = 1
-        vim.g.mkdp_refresh_slow = 0
-        vim.g.mkdp_command_for_global = 0
-        vim.g.mkdp_open_to_the_world = 0
-        vim.g.mkdp_open_ip = ''
-        vim.g.mkdp_browser = ''
-        vim.g.mkdp_echo_preview_url = 0
-        vim.g.mkdp_browserfunc = ''
-    end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
 }
